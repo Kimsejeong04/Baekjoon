e5aeafc8ba68445c612bb981d0a7d385a7e8f51d
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2025-12-10 18:49:40 +0900

    Bronze_3 배수와 약수.js

--- node.js/백준/Bronze/5086. 배수와 약수/배수와 약수.js
+++ node.js/백준/Bronze/5086. 배수와 약수/배수와 약수.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').toString().trim().split(/\s+/).map(Number);
+const input = fs.readFileSync('input.txt').toString().trim().split(/\s+/).map(Number);  //? .split(/\s+/): 줄바꿈이든 뛰어쓰기든 상관없이 모든 공백을 기준으로 잘라냄
 
-for(i = 0; i < input.length - 2; i += 2){
-   const A = input[i];
-   const B = input[i + 1];
+//? i가 1부터 시작하여, 끝에서 두번째 전까지 돎(0 0은 끝을 표현한 것이라 생각)
+//? i += 2: 숫자 두개가 한쌍이므로 한 번에 두칸씩 점프
+for(i = 0; i < input.length - 2; i += 2){ 
+   const A = input[i];  //? 현재 쌍의 첫 번째 숫자 저장(예를 들어 8 16 이면 A는 8, B는 16을 저장) 
+   const B = input[i + 1];  //? 현재 쌍의 두 번째 숫자 저장
 
-   if(B % A === 0){
+   if(B % A === 0){  //? 약수 판별 -> B를 A로 나누었을 때 나머지가 0이면 약수
       console.log('factor');
    }
-   else if(A % B === 0){
+   else if(A % B === 0){  //? 배수 판별 -> A를 B로 나누었을 때 나머지가 0이면 배수 
       console.log('multiple');
    }
-   else{
+   else{  //? 둘 다 아님
       console.log('neither')
    }
 }
