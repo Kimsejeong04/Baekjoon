2c8f9247d310ba58790cae6bdc29cdf7d6a13a8a
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-01-26 21:10:11 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').toString().trim();
+
+/* MenOfPassion(A[], n) {
+    i = ⌊n / 2⌋;
+    return A[i]; # 코드1
+} */
+
+console.log(1);
+console.log(0);
