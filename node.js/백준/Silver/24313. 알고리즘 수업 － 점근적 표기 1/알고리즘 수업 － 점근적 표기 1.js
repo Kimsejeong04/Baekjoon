e92e1054defd406074c97c4110b4a5a5de15618a
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-02-05 14:56:20 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 88 ms, Memory: 9344 KB -BaekjoonHub
    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 88 ms, Memory: 9344 KB -BaekjoonHub

--- node.js/백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.js
+++ node.js/백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
+const input = fs.readFileSync('input.txt').toString().trim().split('\n');
 
-const a0_a1 = input[0].split(' ').map(Number);
-const a0 = a0_a1[1];
-const a1 = a0_a1[0];
+const a0_a1 = input[0].split(' ').map(Number);  //? 첫번째 줄 데이터 처리
+const a0 = a0_a1[1];  //? f(n)의 기울기
+const a1 = a0_a1[0];  //? f(n)의 상수항
 
-const c = Number(input[1]);
-const n0 = Number(input[2]);
+const c = Number(input[1]);  //? g(n)의 기울기
+const n0 = Number(input[2]);  //? 비교를 시작할 기준점 n
 
+//? 조건 1: (a1 * n0 + a0 <= c * n0) -> 시작점(n0)에서 f(n)이 c*g(n)보다 작거나 같아야 한다
+//? 조건 2: (a1 <= c) -> f(n)의 기울기가 c보다 작거나 같아야 역전되지 않는다
 if(a1 * n0 + a0 <= c * n0 && (a1 <= c)){
-    console.log(1);
+    console.log(1);  //? 두 조건을 모두 만족할 시 O(n) 정의 부합
 }
 else{
-    console.log(0);
+    console.log(0);  //? 하나라도 만족하지 않으면 정의에 부합되지 않음
 }
